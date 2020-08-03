fca0fa99a864f714eb5ef948f7ff05f3b552b550
nadya <nitter@rtech.ru>
2020-08-03 11:44:44 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import com.google.gson.Gson;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public void testFunc() {
+        Gson gson = new Gson();
+        RequestSpecification request = RestAssured.given();
+        try {
+            Connection conn = DriverManager.getConnection("url", "user", "password");
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+}
